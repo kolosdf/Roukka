ac256218bee2b8a795914fbf1e4139fc90c35d8e
JuanCardona97 <62678264+JuanCardona97@users.noreply.github.com>
2020-10-13 12:00:56 -0500

    Revert "Juan d"

--- srcgui/src/Routes.js
+++ srcgui/src/Routes.js
@@ -9,22 +9,11 @@ import { LeftSidebar, PresentationLayout } from './layout-blueprints';
 
 
 
-//Landing Page Roukka
-import Plan from './containers/LandingPageRoukka/PlanComponent'
-import Header from './containers/LandingPageRoukka/HeaderLandingPage'
-import Contact from './containers/LandingPageRoukka/ContactComponent'
-import About from './containers/LandingPageRoukka/AboutComponent'
-import ComprarPlan from './containers/LandingPageRoukka/ComprarPlan'
 
-//Admin Roukka
-
-import LeftAdminRoukka from './containers/AdminRoukka/LeftAdminRoukka'
-import MPlanRoukka from './containers/AdminRoukka/MPlanRoukka'
-import MUserRoukka from './containers/AdminRoukka/MUserRoukka'
-import MClientRoukka from './containers/AdminRoukka/MClientRoukka'
-import MDashboardRoukka from './containers/AdminRoukka/MDashboardRoukka'
-
-
+import Plan from './Components/PlanComponent'
+import Header from './Components/HeaderLandingPage'
+import Contact from './Components/ContactComponent'
+import About from './Components/AboutComponent'
 // Example Pages
 
 import Buttons from './example-pages/Buttons';
@@ -46,7 +35,8 @@ const DashboardDefault = lazy(() => import('./example-pages/DashboardDefault'));
 const Cards3 = lazy(() => import('./example-pages/Cards3'));
 const ListGroups = lazy(() => import('./example-pages/ListGroups'));
 const LandingPage = lazy(() => import('./example-pages/LandingPage'));
-const Home = lazy(() => import ('./containers/LandingPageRoukka/HomeComponent')) ;
+const LandingPagePri = lazy(() => import ('./Components/LandingPage')) ;
+const Home = lazy(() => import ('./Components/HomeComponent')) ;
 const Modals = lazy(() => import('./example-pages/Modals'));
 const Notifications = lazy(() => import('./example-pages/Notifications'));
 const Carousels = lazy(() => import('./example-pages/Carousels'));
@@ -89,16 +79,14 @@ const Routes = () => {
               Please wait while we load the live preview examples
             </div>
           </div>
-        }>  
+        }>
+         
         <Switch>
-        
           <Redirect exact from="/" to="/Home" />
-          
-          <Route path={['/LandingPage', '/Home', '/Plans','/Contactus','/Aboutus','/Comprar']}>
+          <Route path={['/LandingPage', '/Home', '/Plans','/Contactus','/Aboutus']}>
             <PresentationLayout>
-              <Header />
+            <Header/>
               <Switch location={location} key={location.pathname}>
-              
                 <motion.div
                   initial="initial"
                   animate="in"
@@ -109,7 +97,7 @@ const Routes = () => {
                   <Route path="/Plans" component={Plan} />
                   <Route path="/Contactus" component={Contact} />
                   <Route path="/Aboutus" component={About} />
-                  <Route path="/Comprar" component={ComprarPlan} />
+                  <Route path="/LandingPage" component={LandingPagePri} />
                   
                 </motion.div>
               </Switch>
@@ -186,44 +174,6 @@ const Routes = () => {
               </Switch>
             </LeftSidebar>
           </Route>
-
-          {
-          //Admin Roukka
-          }
-          <Route path={['/DashboardRoukka','/PlanRoukka', '/ClientRoukka', '/UserRoukka']}>
-            <LeftAdminRoukka>
-
-            <Switch location={location} key={location.pathname}>
-            <motion.div
-                  initial="initial"
-                  animate="in"
-                  exit="out"
-                  variants={pageVariants}
-                  transition={pageTransition}>
-                  <Route
-                    path="/DashboardRoukka"
-                    component={MDashboardRoukka}
-                  />
-                  <Route
-                    path="/PlanRoukka"
-                    component={MPlanRoukka}
-                  />
-                  <Route
-                    path="/ClientRoukka"
-                    component={MClientRoukka}
-                  />
-                  <Route
-                    path="/UserRoukka"
-                    component={MUserRoukka}
-                  />
-              </motion.div>
-            </Switch>
-
-
-            </LeftAdminRoukka>
-            
-          </Route>
-
         </Switch>
       </Suspense>
     </AnimatePresence>
--- srcgui/src/config/configureStore.js
+++ srcgui/src/config/configureStore.js
@@ -1,12 +1,11 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import reducers from '../reducers';
-import thunk from 'redux-thunk'
 
 export default function configureStore() {
   return createStore(
     combineReducers({
       ...reducers
     }),
-    applyMiddleware(thunk)
+    {}
   );
 }
