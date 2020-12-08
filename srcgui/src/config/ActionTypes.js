10bddb411187fefa46e6420d5e9bed56b59716e5
seibern <melissa.fuentes@correounivalle.edu.co>
2020-12-07 23:47:20 -0500

    arreglando errores al hacer merge

--- srcgui/src/config/ActionTypes.js
+++ srcgui/src/config/ActionTypes.js
@@ -87,15 +87,15 @@ export const CREATE_MESSAGE = 'CREATE_MESSAGE'
 
 
 //Types de Login
-//export const USER_LOADING = 'USER_LOADING'
-//export const USER_LOADED = 'USER_LOADED'
-//export const AUTH_ERROR = 'AUTH_ERROR'
+export const USER_LOADING = 'USER_LOADING'
+export const USER_LOADED = 'USER_LOADED'
+export const AUTH_ERROR = 'AUTH_ERROR'
 
 
-//export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
-//export const LOGIN_FAIL = 'LOGIN_FAIL'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAIL = 'LOGIN_FAIL'
 
-//export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
 
 
 //FACTURA
--- srcgui/src/index.js
+++ srcgui/src/index.js
@@ -5,26 +5,10 @@ import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
-const theme = createMuiTheme({
-
-  palette:{
-  
-    primary:{
-      main: '#212121'
-    },
-    secondary:{
-      main: '#ffc107'
-    }
-  
-  }
-
-})
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
-    <App />
-    </ThemeProvider>
-    , document.getElementById('root'));
+
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
